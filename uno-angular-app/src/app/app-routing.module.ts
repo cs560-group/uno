8a722d588521c99e4c475ce9949d099c09a9df3f
Min Namgung <39498561+MinNamgung@users.noreply.github.com>
2020-04-12 21:33:12 -0400

    Add two more pages, fix all the game components

--- uno-angular-app/src/app/app-routing.module.ts
+++ uno-angular-app/src/app/app-routing.module.ts
@@ -3,9 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LobbyComponent } from "@app/components/lobby/lobby.component";
 import { GameComponent } from '@app/components/game/game.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import { firstpageComponent } from './components/firstpage/firstpage.component';
+import { secondpageComponent } from './components/secondpage/secondpage.component';
 
 const routes: Routes = [
-  { path: "", component: RegistrationComponent },
+  { path: "", component: firstpageComponent },
+  { path: "mode", component: secondpageComponent },
+  { path: "register", component: RegistrationComponent },
   { path: "lobby/:username", component: LobbyComponent },
   { path: "game/:id", component: GameComponent },
 ];
