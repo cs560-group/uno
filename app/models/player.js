5a7d5c069386c7be134728454651d5522dfc6309
norman-ma <mikexcelsior@gmail.com>
2020-04-16 15:16:47 -0400

    bots-init

--- app/models/player.js
+++ app/models/player.js
@@ -54,4 +54,77 @@ class Player{
     }
 }
 
-module.exports = Player;
+class Bot extends Player{
+    constructor(name, game, difficulty){
+        super(null, name);
+        this.game = game;
+        this.difficulty = difficulty;
+        this.isBot = true;
+    }
+
+    getPlayableCards(){
+        let playableCards = []
+        for(let card of this.hand.cards){
+            if(this.game.isPlayable(card)){
+                playableCards.push(card)
+            }
+        }
+        return playableCards
+    }
+
+    playCard(){
+        if(this.myTurn){
+            let playableCards = this.getPlayableCards()
+            if(playableCards.length === 0){
+                return false;
+            }else{
+                if(this.difficulty === 1){
+                    playableCards.push(false);
+                    let card = this.playableCards[Math.floor(Math.random() * playableCards.length)]
+                    return card
+                }else if(this.difficulty === 2){
+                    playableCards.sort((a,b) => {return b.points - a.points})
+                    let card
+                    if(playableCards.length > 1){
+                        card = this.playableCards[Math.floor(Math.random() * playableCards.length / 2)]
+                    }else{
+                        card = this.playableCards[0]
+                    }
+                    return card                
+                }else if(this.difficulty === 3){
+                    playableCards.sort((a,b) => {return b.points - a.points})
+                    let card = playableCards[0]
+                    if(card.value === "+4" && playableCards.length > 1){
+                        card = playableCards[1]
+                    }
+                    return card
+                }
+            }
+        }
+    }
+
+    challenge(){
+        if(this.myTurn){
+            if(this.game.discard.getTopCard().value === "+4"){
+                if(this.difficulty === 1){
+                    return true
+                }else if(this.difficulty === 2){
+                    let choice = [true, false];
+                    return choice[Math.floor(Math.random() * choice.length)]
+                }else if(this.difficulty === 3){
+                    let prevPlayer = this.game.direction === 0 ? this.right : this.left
+                    if(prevPlayer.hand.count() > 4){
+                        return true
+                    }else{
+                        return false
+                    }
+                }
+            }
+        }
+    }
+}
+
+module.exports = {
+    Player: Player,
+    Bot: Bot
+};
