8899311e0c08469266d6ef9f6a1ba1a3d46a972e
norman-ma <mikexcelsior@gmail.com>
2020-04-17 07:42:20 -0400

    bot-player update

--- app/models/player.js
+++ app/models/player.js
@@ -64,9 +64,10 @@ class Bot extends Player{
 
     getPlayableCards(){
         let playableCards = []
-        for(let card of this.hand.cards){
+        for(let i = 0; i < this.hand.cards.length; i++){
+            let card = this.hand.getCard(i)
             if(this.game.isPlayable(card)){
-                playableCards.push(card)
+                playableCards.push({card: card, index: i})
             }
         }
         return playableCards
@@ -75,32 +76,108 @@ class Bot extends Player{
     playCard(){
         if(this.myTurn){
             let playableCards = this.getPlayableCards()
+
             if(playableCards.length === 0){
                 return false;
             }else{
-                if(this.difficulty === 1){
-                    playableCards.push(false);
-                    let card = this.playableCards[Math.floor(Math.random() * playableCards.length)]
-                    return card
-                }else if(this.difficulty === 2){
-                    playableCards.sort((a,b) => {return b.points - a.points})
-                    let card
-                    if(playableCards.length > 1){
-                        card = this.playableCards[Math.floor(Math.random() * playableCards.length / 2)]
-                    }else{
-                        card = this.playableCards[0]
-                    }
-                    return card                
-                }else if(this.difficulty === 3){
-                    playableCards.sort((a,b) => {return b.points - a.points})
-                    let card = playableCards[0]
-                    if(card.value === "+4" && playableCards.length > 1){
-                        card = playableCards[1]
-                    }
-                    return card
+                
+                let card = this.chooseCard()
+
+                if(card.card.isWild){
+                    let color = this.chooseColor()
+                    this.game.playCard(card.index, color)
+                }else{
+                    this.game.playCard(card.index)
                 }
             }
         }
+    }
+
+    chooseCard(){
+        let card
+
+        if(this.game.currentPlayerHasPassed){
+            card = {card: this.hand.getCard(this.hand.count() - 1), index: this.hand.count() -1}
+        }else{
+            if(this.difficulty === 1){
+                playableCards.push(false);
+                card = this.playableCards[Math.floor(Math.random() * playableCards.length)]
+            }else if(this.difficulty === 2){
+                playableCards.sort((a,b) => {return b.points - a.points})
+                if(playableCards.length > 1){
+                    card = this.playableCards[Math.floor(Math.random() * playableCards.length / 2)]
+                }else{
+                    card = this.playableCards[0]
+                }               
+            }else if(this.difficulty === 3){
+                playableCards.sort((a,b) => {return b.points - a.points})
+                let i = 0;
+                card = playableCards[i]
+                while(card.value === "+4" && playableCards.length > 1){
+                    i++
+                    card = playableCards[i]
+                }
+            }
+        }
+
+        return card
+    }
+
+    getColors(){
+        let red = {color: 'red', count: 0, points: 0}
+        let green = {color: 'green', count: 0, points: 0}
+        let yellow = {color: 'yellow', count: 0, points: 0}
+        let blue = {color: 'blue', count: 0, points: 0}
+        let colors = [red, green, yellow, blue]
+
+        for(let card of this.hands.card){
+            let color = card.suit
+            if(color === 'red'){
+                red.count ++
+                red.points += card.points
+            }else if(color === 'green'){
+                green.count ++
+                green.points += card.points
+            }else if(color === 'yellow'){
+                yellow.count ++
+                yellow.points += card.points
+            }else if(color === 'blue'){
+                blue.count ++
+                blue.points += card.points
+            }
+        }
+
+        return colors
+    }
+
+    chooseColor(){
+
+        let colors = this.getColors()
+
+        let currentColor = this.game.discard.getTopCard().suit
+
+        if(this.difficulty === 1){
+            let color = colors[Math.floor(Math.random() * colors.length)]
+            while(color.color === currentColor){
+                color = colors[Math.floor(Math.random() * colors.length)]
+            }
+        }else if(this.difficulty === 2){
+            colors.sort((a,b) => {b.count - a.count})
+            let color = colors[Math.floor(Math.random() * colors.length / 2)]
+            while(color.color === currentColor){
+                color = colors[Math.floor(Math.random() * colors.length / 2)]
+            }
+        }else if(this.difficulty === 3){
+            colors.sort((a,b) => {b.count * b.points - a.count * a.points})
+            let i = 0
+            color = colors[i].color
+            while(color.color === currentColor){
+                i++
+                color = colors[i].color
+            }
+        }
+
+        return color
     }
 
     challenge(){
